1185f164bbb9621f49c7f661953c7961d137ae78
1500371773@qq.com <1500371773@qq.com>
2024-12-26 22:41:35 +0800

    waterfall vue 初始化

---
+++ 瀑布流/VueDemo/src/App.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import WaterFall from './components/WaterFall.vue';
+import data1 from './data/data1.json'
+import data2 from './data/data2.json'
+
+type RawData = typeof data1
+
+const data = [...data1.data.items, ...data2.data.items] as RawData['data']['items']
+
+const loadData = async () => new Promise((resolve) => {
+  setTimeout(() => {
+    resolve(data)
+  }, 1000)
+})
+
+</script>
+
+<template>
+  <div>
+    <WaterFall :load-data="loadData"/>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ 瀑布流/VueDemo/src/components/WaterFall.vue
@@ -0,0 +1,29 @@
+<script setup lang='ts'>
+import { useAsyncState } from '@vueuse/core'
+
+type P = {
+  loadData: () => Promise<any>
+  gap?: number
+}
+
+const {
+  loadData
+} = withDefaults(defineProps<P>(), {
+  gap: 20
+})
+
+const { state, isLoading } = useAsyncState(loadData, {})
+
+</script>
+
+<template>
+  <div>
+    water-fall
+    <div v-if="isLoading">loading</div>
+    <div v-for="item in state" :key="item.id">{{ item.id }}</div>
+  </div>
+</template>
+
+<style>
+
+</style>
